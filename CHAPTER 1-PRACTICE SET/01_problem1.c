65352019167d70e4d18f480aac3fbda16dd26737
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-05-28 00:04:57 +0600

    chapter 01 pratice test upload

---
+++ CHAPTER 1-PRACTICE SET/01_problem1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+
+    int LENGHTM,WIDTH;
+    printf("ENTER LENGTH\n");
+    scanf("%d", &LENGHTM );
+    printf("ENTER WIDTH\n");
+    scanf("%d", &WIDTH );
+    
+    printf("the retengle lenght is %d", LENGHTM*WIDTH);
+    return 0;
+}
