af52045d7bf9d46bf3a0aae90d64d6201e7e10a1
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-05-27 22:28:21 +0600

    add 04 size of c and 05 comment of c

---
+++ chapter-1/05_comments.c
@@ -0,0 +1,13 @@
+/*
+this multilien comment.
+*/
+#include <stdio.h>
+
+int main(){
+
+    //   I am writing for fun. This line dosent do anything
+
+    printf("Hello Monir");
+
+       return 0;  //this is return
+}
