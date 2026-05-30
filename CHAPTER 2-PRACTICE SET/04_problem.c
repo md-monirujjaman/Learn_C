c97d726c76df90189177fc6b9dac394d61d5a9a5
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-05-30 21:34:39 +0600

    chapter 2 problem 4 solve

---
+++ CHAPTER 2-PRACTICE SET/04_problem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    //  Explain step by step evaluation of 3*x/y – z+k, where x=2, y=3, z=3, k=1
+    float x = 2, y = 3, z = 3, k = 1;
+    float e = 3 * x / y - z + k;
+    // 3 * x / y - z + k
+    // // 6/ y - z + k
+    // // 2- z + k
+    // // -1+k
+    // // -1+1
+    // // 0
+
+    printf("--------------------------------\n"); // This is for decoration nothing else.
+    printf("The value of e is %d\n", e);
+    printf("--------------------------------\n");
+
+    
+    return 0;
+}
