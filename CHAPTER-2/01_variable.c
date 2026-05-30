2a7668e96ad8a6996cb9307501c62ef0a71e66ca
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-05-30 19:49:29 +0600

    chapter 2 create and 01_variable.c done

---
+++ CHAPTER-2/01_variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+     int i =10; //Delare and intiialize 'i' with 10
+     int j =i;
+     // %d is called formato specifier
+     // %d is for int , %f ir for float and %c is for char
+     int a =2, b=3, c=4, d=5;
+     printf("the value of i is %d and the value of j is %d\n",i,j);
+     printf("the value of a is %d and the value of b is %d\n",a,b);
+     printf("the value of c is %d and the value of d is %d\n",c,d);
+    return 0;
+}
