50c784556736be253343cf8cca56782c08ce08a4
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-05-30 20:36:38 +0600

    03_type_convarstion.c Done

---
+++ CHAPTER-2/03_types_converstion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 9.0;
+    int b = 2;
+    float c = a / b;
+    printf("--------------------------------\n");
+    printf("The value of a/b is %f\n", c);
+    printf("--------------------------------\n");
+
+    // int and int       =  int
+    // int and float     =  float
+    // float and float   =  float
+
+    return 0;
+}
