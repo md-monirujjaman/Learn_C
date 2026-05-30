d35c4a642073887a76f8cb2607ee97ab1043922e
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-05-30 20:18:19 +0600

    02_arthmetic_operators.c

--- CHAPTER-2/02_arithmetic_operators.c
+++ CHAPTER-2/02_arithmetic_operators.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main (){
-    int a =5;
-    int b=3;
-    int c=a+b;
-    printf("the value of a is %d and value of b is %d and sum is %d\n",a,b,c);
-    //Modulus operator is used to get the remainder
-    printf("the reaminder when a is divided by b is: %d\n",a%b);
+int main()
+{
+    int a = 5;
+    int b = 3;
+    int c = a + b;
+    printf("the value of a is %d and value of b is %d and sum is %d\n", a, b, c);
+    // Modulus operator is used to get the remainder
+    printf("the reaminder when a is divided by b is: %d\n", a % b);
+    // this does not work for exponentiation in c
+    // int d = a^b;
     return 0;
 }
