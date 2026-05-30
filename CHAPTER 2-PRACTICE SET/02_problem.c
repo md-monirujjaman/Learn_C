11e844936820226c89c2727f60d6055be1f2844f
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-05-30 21:17:34 +0600

    chapter 2 promblem 3 solve

---
+++ CHAPTER 2-PRACTICE SET/02_problem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    // What data type will 3.0/8 – 2 ret
+    float a = 3.0/8 - 2;
+
+    printf("--------------------------------\n"); // This is for decoration nothing else.
+    printf("The value is:%f\n",a);
+     printf("--------------------------------\n");
+    return 0;
+}
