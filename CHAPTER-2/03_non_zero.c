16bd66a04074119b2462c2898295fc97776d3948
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-06-01 21:35:18 +0600

    03 non zero.c

---
+++ CHAPTER-2/03_non_zero.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main (){
+    if (2)
+    {
+        printf("The code is executedbeacue if is int\n");
+
+    }
+    if (2234)
+    {
+     printf("The code is executed beacue if is int\n");
+    }
+
+    if (2.566)
+    {
+        printf("The code is executed beacue if is float\n");
+    }
+    if ('c')
+    {
+        printf("The code is executed beacue if is char\n");
+    }
+    
+    if (0)
+    {
+        printf("The code is not  executed.Reason is 0\n");
+    }
+    
+    
+    
+    
+    return 0;
+}
---
+++ CHAPTER-2/tempCodeRunnerFile.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (){
+    if (2)
+    {
+        printf("The code is executed\n");
+
+    }
+    if (2234)
+    {
+     printf("The code is executed\n");
+    }
+
+    if (2.566)
+    {
+        printf("The code is executed\n");
+    }
+
+    if (0)
+    {
+        printf("The code is not  executed.Reason is 0\n");
+    }
+    
+    
+    
+    
+    return 0;
+}
