3e51c1f2e69cb60aac43a8e2bc6d0fec2b36a849
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-06-01 21:08:52 +0600

    chapter 3 start and upload 01 condional.c

---
+++ chapter-3/01_conditionals.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int age =19;
+    if (age>18)
+    {
+        printf("your age is gather then 18\n");
+        printf("-------------------------------\n"); //this is for decoration
+        printf("you have chance to get a passport");
+    }
+    
+    return 0;
+}
