cb801752d791e492b06225a807a3b1e9dac046d4
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-06-02 07:12:06 +0600

    06_ternary.c upload

---
+++ chapter-3/06_ternary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // condition ? expression-if-true : expression-if-false
+    int a = 44;
+    int b = 55;
+    a > b ? printf("a is gatherthen") : printf("b is less then");
+    return 0;
+}
